bd9417e9cf21b7156d2071c6c9733b2a6e1517bf
Haine Cho <hainec@andrew.cmu.edu>
2019-07-17 17:16:33 -0400

    changes savedto database

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
+import {startSetExpenses} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 //import './playground/promises';
@@ -24,4 +24,9 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
